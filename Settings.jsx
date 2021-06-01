11acd85a0ca1d7b2c084fe34a186abaaaf684f58
e-boi <hiboysismyoufake6789@gmail.com>
2021-06-01 10:34:33 -0400

    have it be able to set to 0

--- Settings.jsx
+++ Settings.jsx
@@ -6,10 +6,10 @@ module.exports = class AmongSettings extends React.PureComponent {
 		const { getSetting, updateSetting } = this.props;
 		return (
 			<SliderInput
-				minValue={1}
+				minValue={0}
 				maxValue={100}
 				initialValue={getSetting('volume', 1) * 100}
-				markers={[1, 50, 100]}
+				markers={[0, 50, 100]}
 				onValueChange={change => {
 					updateSetting('volume', change / 100);
 					window.sus.volume = change / 100;
